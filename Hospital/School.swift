636bf4ad52b83bae9fee3f9562075836ad04c000
Ian Alexander Rahman <ianrahman@users.noreply.github.com>
2016-09-20 18:03:50 -0400

    Updated for Swift 3

--- Hospital/School.swift
+++ Hospital/School.swift
@@ -1,13 +1,7 @@
 //
 //  School.swift
-//  Hospital
+//  School
 
-
-enum Punishment {
-    case Severe
-    case Light
-    case Medium
-}
 
 protocol Payable {
     func wages() -> Double
@@ -26,12 +20,16 @@ protocol TimeOff {
 }
 
 extension TimeOff {
-    
     func requestForVacation(days: Int) -> Bool {
-        guard vacationDays > 0 else { return false }
-        return vacationDays >= days
+        return days <= vacationDays
     }
-    
+}
+
+
+enum Punishment {
+    case severe
+    case light
+    case medium
 }
 
 protocol Reprimand {
@@ -40,49 +38,44 @@ protocol Reprimand {
 }
 
 extension Reprimand {
-    
     func disciplineStudent(scale: Punishment) -> String {
         switch scale {
-        case .Severe: return "We are expelling you from the school!"
-        case .Light: return "You're to go back to class, don't do it again."
-        case .Medium: return "Why would you think that's a good idea?! You're going to detention."
+        case .severe: return "We are expelling you from the school!"
+        case .light: return "You're to go back to class, don't do it again."
+        case .medium: return "Why would you think that's a good idea?! You're going to detention."
         }
     }
     
     func callParentDeliveringMessage(scale: Punishment) -> String {
         switch scale {
-        case .Severe: return "Your child has been expelled from the school."
-        case .Light: return "Your child thought it was funny to put gum in Amy's hair."
-        case .Medium: return "Your child has become a terror in the classroom."
+        case .severe: return "Your child has been expelled from the school."
+        case .light: return "Your child thought it was funny to put gum in Amy's hair."
+        case .medium: return "Your child has become a terror in the classroom."
         }
     }
-    
 }
+
 
 enum Subject {
-    case Math, Science, English
+    case math
+    case science
+    case english
 }
-
 
 protocol Teach {
     func teachSubject(subject: Subject) -> String
 }
 
 extension Teach {
-    
     func teachSubject(subject: Subject) -> String {
         switch subject {
-        case .Math: return "Take out your math books please."
-        case .Science: return "Time to learn the best subject of all! Science!!"
-        case .English: return "To read or not to read. Everyone take out your english books."
+        case .math: return "Take out your math books please."
+        case .science: return "Time to learn the best subject of all! Science!!"
+        case .english: return "To read or not to read. Everyone take out your english books."
         }
     }
 }
 
 
 protocol Employee: Payable, TimeOff { }
-
-
-
-
 
